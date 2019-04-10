5a54bb56ccd9729374ac77a5eb486788d0634472
v-makeev <curage6699@yandex.ru>
2019-04-10 20:45:11 +0300

    main.cpp deleted

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "src/parser.h"
-
-int main() {
-    JsonParser::Dictionary a;
-    a.parse(R"({"name":"Ivan","age":"18"})");
-    std::cout << a.to_string() << std::endl;
-    return 0;
-}
